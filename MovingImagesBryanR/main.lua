5fc9d6dd134d5c5796c48f6076a2eecc330b05c5
Bryan Rodriguez <47784159+s267522@users.noreply.github.com>
2019-02-26 09:46:01 -0500

    updated displayshapes

--- MovingImagesBryanR/main.lua
+++ MovingImagesBryanR/main.lua
@@ -35,7 +35,29 @@ local function MoveShip(event)
 	beetleship.x = beetleship.x + scrollSpeed
 	-- change the transparency of the ship every time 
 	-- it moves so that it fades out
+	beetleship.alpha = beetleship.alpha + 0.01
 end
 
 -- Moveship will be called over and over again
-Runtime:addEventListener("enterFrame", Moveship)
+Runtime:addEventListener("enterFrame", MoveShip)
+
+--create star
+local star = display.newImageRect("Images/star.png", 200, 200)
+
+-- set initial x and y position of star
+star.x = display.contentWidth/0.9
+star.y = display.contentHeight/2
+
+-- Function: MoveStar
+-- Input: this function accepts an event listener
+-- Output: none
+-- Description: This function adds the scroll speed to the x-value of the star
+local function MoveStar(event)
+	-- add scroll speed to the x-value of the star
+	star.x = star.x - scrollSpeed
+	-- change the transparency of the ship every time it moves so that it fades out
+	star.alpha = star.alpha + 0.01
+end
+
+-- MoveStar will be called over and over again
+Runtime:addEventListener("enterFrame", MoveStar)
